5aa8b197264d723740d989e94da39934b06f4939
Shivam Kshirsagar <kshirsagarshivam33@gmail.com>
2025-09-29 20:17:33 +0530

    Update ArrayList1.java

--- ArrayList1.java
+++ ArrayList1.java
@@ -4,13 +4,9 @@ public class ArrayList1 {
     public static void main(String[] args) {
         ArrayList <Integer> list = new ArrayList<>();
         System.out.println("Hello World");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int a = sc.nextInt();
-        System.out.println(a); 
-        sc.close();
     }
 }
+
 
 
 
